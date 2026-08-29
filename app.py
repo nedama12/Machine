e51dc39003518806ff5f99027cdf02b9f454c514
nedama12 <neiderfelipe2003@gmail.com>
2026-08-29 07:26:21 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/definition")
+def definition():
+    return render_template("definition.html")
+
+
+@app.route("/types")
+def types():
+    return render_template("types.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
